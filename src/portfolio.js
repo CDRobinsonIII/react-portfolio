be7a01fb47414d92b7697a0467d77417a7b560b4
Charles Robinson <charlesdalerobinson@gmail.com>
2021-03-04 23:24:32 -0800

    Added 2 more projects to portfolio and made some CSS changes.

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,18 +1,18 @@
 export const data = [
   {
     "id": 1,
-    "portfolioTitle": "The Brewway",
-    "portfolioScreenShot": require("./images/the_brewway.jpg"),
-    "portfolioGitHubLink": "https://github.com/CDRobinsonIII/brewery-tour",
-    "portfolioDeployedLink": "https://cdrobinsoniii.github.io/brewery-tour/"
+    "portfolioTitle": "Student Database Portal",
+    "portfolioScreenShot": require("./images/studentportal.jpg"),
+    "portfolioGitHubLink": "https://github.com/CDRobinsonIII/student-database-portal",
+    "portfolioDeployedLink": "https://student-registration-tracker.herokuapp.com/"
   },
 
   {
     "id": 2,
-    "portfolioTitle": "Weather Dashboard",
-    "portfolioScreenShot": require("./images/weather_dashboard.jpg"),
-    "portfolioGitHubLink": "https://github.com/CDRobinsonIII/weather-dashboard",
-    "portfolioDeployedLink": "https://cdrobinsoniii.github.io/weather-dashboard/"
+    "portfolioTitle": "The Brewway",
+    "portfolioScreenShot": require("./images/the_brewway.jpg"),
+    "portfolioGitHubLink": "https://github.com/CDRobinsonIII/brewery-tour",
+    "portfolioDeployedLink": "https://cdrobinsoniii.github.io/brewery-tour/"
   },
 
   {
@@ -25,10 +25,26 @@ export const data = [
 
   {
     "id": 4,
-    "portfolioTitle": "Student Database Portal",
-    "portfolioScreenShot": require("./images/studentportal.jpg"),
-    "portfolioGitHubLink": "https://github.com/CDRobinsonIII/student-database-portal",
-    "portfolioDeployedLink": "https://student-registration-tracker.herokuapp.com/"
-  }
+    "portfolioTitle": "React Employee Directory",
+    "portfolioScreenShot": require("./images/reactemployee.jpg"),
+    "portfolioGitHubLink": "https://github.com/CDRobinsonIII/employee-directory-react",
+    "portfolioDeployedLink": "https://cdrobinsoniii.github.io/employee-directory-react/"
+  },
+
+  {
+    "id": 5,
+    "portfolioTitle": "Weather Dashboard",
+    "portfolioScreenShot": require("./images/weather_dashboard.jpg"),
+    "portfolioGitHubLink": "https://github.com/CDRobinsonIII/weather-dashboard",
+    "portfolioDeployedLink": "https://cdrobinsoniii.github.io/weather-dashboard/"
+  },
+
+  {
+    "id": 6,
+    "portfolioTitle": "Timed Quiz",
+    "portfolioScreenShot": require("./images/quiz_challenge.jpg"),
+    "portfolioGitHubLink": "https://github.com/CDRobinsonIII/timed-quiz-utilizing-DOM-JS",
+    "portfolioDeployedLink": "https://cdrobinsoniii.github.io/timed-quiz-utilizing-DOM-JS/"
+  },
   
 ]
